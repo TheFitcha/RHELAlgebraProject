5d324cf12f4eca567ab843607f1c65ebd6ffdd12
Filip Balder <fbalder@cratis.hr>
2026-06-15 10:14:01 +0200

    Update

--- roles/web/files/mediawiki_db.sql
+++ roles/web/files/mediawiki_db.sql
@@ -1,1 +1,4 @@
-create database mw_db; create user 'mediawiki'@'%' identified by 'mw_password'; grant all on mw_db.* to 'mediawiki'@'%'; flush privileges;
+create database mw_db; 
+create user 'mediawiki'@'%' identified by 'mw_password'; 
+grant all on mw_db.* to 'mediawiki'@'%'; 
+flush privileges;
