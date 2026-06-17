4e0c3253286618900c9e0585b390e6d26f67e941
Filip Balder <fbalder@cratis.hr>
2026-06-17 15:34:59 +0200

    Update

--- roles/web/files/wordpress_config.php
+++ roles/web/files/wordpress_config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'wordpress' );
 define( 'DB_PASSWORD', 'wp_password' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', '172.25.250.11' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
