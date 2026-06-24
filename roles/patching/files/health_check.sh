2d94b0b25e0fc3dee2cb1a8d8110395c8684a80e
Filip Balder <fbalder@cratis.hr>
2026-06-24 13:32:34 +0200

    Update

--- roles/patching/files/health_check.sh
+++ roles/patching/files/health_check.sh
@@ -2,4 +2,4 @@
 
 echo "$(hostname) - $(uptime) - $(df -h)" > /var/log/nexcore-health.log
 
-cat /var/log/nexcore-health.log | mail -s "Daily server health status" ops01@nexcore.local
+cat /var/log/nexcore-health.log | mail -s "Daily server check - $(date)" ops01@nexcore.local
