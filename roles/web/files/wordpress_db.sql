40baefe024a6a1f05211f9dec27c3e3cddb152aa
Filip Balder <fbalder@cratis.hr>
2026-06-26 09:52:28 +0200

    Update

--- roles/web/files/wordpress_db.sql
+++ roles/web/files/wordpress_db.sql
@@ -1,4 +1,4 @@
 create database if not exists wp_db;
 create user if not exists wordpress identified by 'wp_password';
-grant all on wp_db.* to 'wordpress'@'%';
+grant all privileges on wp_db.* to 'wordpress'@'172.25.250.%' identified by 'wp_password' with grant option;
 flush privileges;
