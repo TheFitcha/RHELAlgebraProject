8f5cc37b0311cfe7b080609cb61ab8ecbac75c1f
Filip Balder <fbalder@cratis.hr>
2026-06-26 12:17:05 +0200

    Update

--- nexcore-infra/roles/web/files/mediawiki_db.sql
+++ nexcore-infra/roles/web/files/mediawiki_db.sql
@@ -1,4 +1,4 @@
 create database if not exists mw_db; 
 create user if not exists 'mediawiki'@'%' identified by 'mw_password'; 
-grant all privileges on mw_db.* to 'mediawiki'@'172.25.250.%' identified by 'mw_password' with grant option;
+grant all privileges on mw_db.* to 'mediawiki'@'%' identified by 'mw_password' with grant option;
 flush privileges;
--- nexcore-infra/roles/web/files/wordpress_db.sql
+++ nexcore-infra/roles/web/files/wordpress_db.sql
@@ -1,4 +1,4 @@
 create database if not exists wp_db;
 create user if not exists wordpress identified by 'wp_password';
-grant all privileges on wp_db.* to 'wordpress'@'172.25.250.%' identified by 'wp_password' with grant option;
+grant all privileges on wp_db.* to 'wordpress'@'%' identified by 'wp_password' with grant option;
 flush privileges;
